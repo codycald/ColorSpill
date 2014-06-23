b2d16ef9176a02e82eb1ce6df05ff28a25da64d8
Cody Caldwell <cocaldwe@gmail.com>
2014-06-23 03:43:07 -0600

    Implemented a basic scrolling menu system in the photo editor

--- FilterLab/FLTPhotoEditorViewController.m
+++ FilterLab/FLTPhotoEditorViewController.m
@@ -9,12 +9,21 @@
 #import "FLTPhotoEditorViewController.h"
 #import "GPUImage.h"
 
-@interface FLTPhotoEditorViewController ()
+@interface FLTPhotoEditorViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (strong, nonatomic) UICollectionView *currentMenu;
+@property (assign, nonatomic) NSInteger menuPadding;
 
 @property (weak, nonatomic) IBOutlet GPUImageView *originalImageView;
 @property (weak, nonatomic) IBOutlet GPUImageView *filteredImageView;
+@property (weak, nonatomic) IBOutlet UIToolbar *editingToolBar;
 
 @end
+
+typedef enum {
+    kFilterMenu = 1,
+    kToolMenu = 2
+} MenuType;
 
 @implementation FLTPhotoEditorViewController
 
@@ -22,7 +31,7 @@
     
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // 
+        self.menuPadding = 10;
     }
     return self;
 }
@@ -42,6 +51,39 @@
     
     [self.originalImageView setHidden:YES];
     [self.filteredImageView setHidden:NO];
+}
+
+#pragma mark - UICollectionView data source methods
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionViewCell *cell = [collectView dequeueReusableCellWithReuseIdentifier:@"UICollectionViewCell"
+                                                                        forIndexPath:indexPath];
+    
+    switch (collectView.tag) {
+        case kFilterMenu:
+            cell.backgroundColor = [UIColor whiteColor];
+            break;
+            
+        case kToolMenu:
+            cell.backgroundColor = [UIColor blueColor];
+            break;
+            
+        default:
+            break;
+    }
+    
+    return cell;
+}
+
+#pragma mark - UICollectionView delegate methods
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"Selected item #%ld", indexPath.row);
 }
 
 #pragma mark - Touch event handlers
@@ -86,6 +128,14 @@
     [self.filteredImageView setHidden:NO];
 }
 
+- (IBAction)showFilterMenu:(id)sender {
+    [self displayMenuOfType:kFilterMenu];
+}
+
+- (IBAction)showToolMenu:(id)sender {
+    [self displayMenuOfType:kToolMenu];
+}
+
 #pragma mark - Helper methods
 
 - (void)reconfigureImageViews {
@@ -115,6 +165,63 @@
     
     [self.originalImageView setInputRotation:rotationMode atIndex:0];
     [self.filteredImageView setInputRotation:rotationMode atIndex:0];
+}
+
+- (void)displayMenuOfType:(MenuType)type {
+    
+    UICollectionView *previousMenu = self.currentMenu;
+    
+    // If there is no menu showing, or if the currently
+    // displayed menu is of a different type than the requested
+    // menu, recreate the menu
+    if (!self.currentMenu || self.currentMenu.tag != type) {
+        
+        [self configureScrollMenuOfType:type];
+        [self.view addSubview:self.currentMenu];
+        
+    } else {
+        self.currentMenu = nil;
+    }
+    
+    // Hide the previous menu if it exists
+    if (previousMenu) {
+        [previousMenu removeFromSuperview];
+    }
+}
+
+- (void)configureScrollMenuOfType:(MenuType)type {
+    
+    UICollectionViewFlowLayout *scrollMenuLayout = [[UICollectionViewFlowLayout alloc] init];
+    scrollMenuLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    scrollMenuLayout.sectionInset = UIEdgeInsetsMake(0, self.menuPadding, 0, self.menuPadding);
+    
+    CGFloat menuItemSize;
+    switch (type) {
+        case kToolMenu:
+            menuItemSize = 50;
+            break;
+        case kFilterMenu:
+            menuItemSize = 30;
+            break;
+    }
+    
+    scrollMenuLayout.itemSize = CGSizeMake(menuItemSize, menuItemSize);
+    
+    self.currentMenu = [[UICollectionView alloc] initWithFrame:CGRectZero
+                                            collectionViewLayout:scrollMenuLayout];
+    [self.currentMenu registerClass:[UICollectionViewCell class]
+           forCellWithReuseIdentifier:@"UICollectionViewCell"];
+    self.currentMenu.dataSource = self;
+    self.currentMenu.delegate = self;
+    self.currentMenu.tag = type;
+    [self.currentMenu setShowsHorizontalScrollIndicator:NO];
+    self.currentMenu.backgroundColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.5];
+    
+    CGFloat scrollMenuHeight = menuItemSize + self.menuPadding * 2;
+    self.currentMenu.frame = CGRectMake(0,
+                                        self.editingToolBar.frame.origin.y - scrollMenuHeight,
+                                        self.view.bounds.size.width,
+                                        scrollMenuHeight);
 }
 
 @end
